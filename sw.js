7db36f8e090ce6ed6a4a37751ed95183121417c9
zeus777ia <5wnwsgtjh4@privaterelay.appleid.com>
2026-08-06 19:12:15 +0300

    SW v17 - force Pages deploy + form (queue unblock)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* KonyaGo SW v16 */
-var CACHE = "konyago-v16";
+/* KonyaGo SW v17 */
+var CACHE = "konyago-v17";
 var PRECACHE = ["./", "./index.html", "./assets/css/app.css", "./assets/js/app.js", "./assets/img/eagle.svg", "./manifest.json"];
 
 self.addEventListener("install", function (e) {
