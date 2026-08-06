5273b71e7f31dac57db42fa412ba83befb8f4826
zeus777ia <5wnwsgtjh4@privaterelay.appleid.com>
2026-08-06 18:51:24 +0300

    SW v14 - force Pages deploy for Bize yazin formu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* KonyaGo SW v13 */
-var CACHE = "konyago-v13";
+/* KonyaGo SW v14 */
+var CACHE = "konyago-v14";
 var PRECACHE = ["./", "./index.html", "./assets/css/app.css", "./assets/js/app.js", "./assets/img/eagle.svg", "./manifest.json"];
 
 self.addEventListener("install", function (e) {
