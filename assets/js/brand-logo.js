0dcf91b39825c4fb89da9c74aabfe366c3bd7a39
zeus777ia <5wnwsgtjh4@privaterelay.appleid.com>
2026-08-12 23:15:15 +0300

    brand: logo injector loads assets/img/logo.jpg with animation

--- assets/js/brand-logo.js
+++ assets/js/brand-logo.js
@@ -1,9 +1,12 @@
 /* KonyaGo brand logo injector */
 (function(){
-  var LOGO = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAQDAwMDAgQDAwMEBAQFBgoGBgUFBgwICQcKFxcZEhMQEyccGh0dHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fHx8fH/2wBDAQQEBAYFBgsGBgsYDw0PGBYYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgY/wAARCAEAAQADASIAAhEBAxEB/8QAHQAAAgIDAQEBAAAAAAAAAAAAAgMBBAUGBwAICf/EAEkQAAIBAwMCBAMFBQUFBgcBAAECAwAEERIhBTFBBhMiUWFxMoEUI0JSkQcVM2KhsRYkcsHwJDTS8BZD4fEWJTM1gqLi/8QAGgEAAwEBAQEAAAAAAAAAAAAAAAECAwQFBv/EAC0RAAICAQMDAwMEAwEAAAAAAAABAhEDEiExBEFRImETcYGRoTKx8PEUwfH/2gAMAwEAAhEDEQA/APooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigAooooAKKKKACiiigD//2Q==";
+  var LOGO = "assets/img/logo.jpg";
   function apply(){
     document.querySelectorAll("img.logo-mark, img.hero-eagle").forEach(function(el){
-      el.src = LOGO;
+      var test = new Image();
+      test.onload = function(){ el.src = LOGO; };
+      test.onerror = function(){ /* keep eagle.svg fallback */ };
+      test.src = LOGO;
       el.alt = el.alt || "KonyaGo";
     });
   }
